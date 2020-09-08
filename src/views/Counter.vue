8c1d6782a0797053efde7d55343f7eaee6963c5e
Andychuang <bboy81905@gmail.com>
2020-09-08 17:37:53 +0800

    Basic Router sample

---
+++ src/views/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="counter">
+    <Header msg="I'm counter Page"></Header>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
