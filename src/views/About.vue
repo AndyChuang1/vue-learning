6c6aef60477d5c6bd8fa3d4a2bde8f4d34a5bbef
Andychuang <bboy81905@gmail.com>
2020-09-08 15:18:10 +0800

    Basic Components sample Header

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Header msg="I'm About Page"></Header>
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Header msg="I'm Home page"></Header>
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>{{ text }}</h1>
   </div>
@@ -7,9 +8,11 @@
 
 <script>
 // @ is an alias to /src
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'Home',
+  components: { Header },
   data() {
     return {
       text: 'Hello world!',
