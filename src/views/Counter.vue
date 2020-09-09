779e3159fe2dd9e041e483471b552fe89e6ef737
Andychuang <bboy81905@gmail.com>
2020-09-09 17:16:02 +0800

    finished computed sample convert integer to roman number

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -2,6 +2,7 @@
   <div class="counter">
     <Header msg="I'm counter Page"></Header>
     <h2 class="text">Number:{{ count }}</h2>
+    <h2 class="text">Roman Number:{{ romanToInt }}</h2>
     <div class="btn-group">
       <button v-on:click="increase()">+</button>
       <button @click="decrease()">-</button>
@@ -25,6 +26,24 @@ export default {
       if (newNumber === 10 || newNumber === 0) {
         alert(`Number achieve ${newNumber}`);
       }
+    },
+  },
+  computed: {
+    romanToInt() {
+      const romanMap = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X',
+      };
+
+      return romanMap[this.count] || this.count;
     },
   },
   methods: {
