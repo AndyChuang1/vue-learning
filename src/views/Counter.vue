da19378cb86ba12925abad988a777e055751aaac
Andychuang <bboy81905@gmail.com>
2020-09-09 13:36:11 +0800

    Event Handling Counter Sample

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="counter">
     <Header msg="I'm counter Page"></Header>
+    <h2 class="text">Number:{{ count }}</h2>
+    <div class="btn-group">
+      <button v-on:click="increase()">+</button>
+      <button @click="decrease()">-</button>
+    </div>
   </div>
 </template>
 <script>
@@ -10,5 +15,29 @@ export default {
   components: {
     Header,
   },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+    decrease() {
+      this.count -= 1;
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+.btn-group {
+  button {
+    width: 30px;
+    font-size: 20px;
+    &:nth-child(even) {
+      margin-left: 0.5rem;
+    }
+  }
+}
+</style>
