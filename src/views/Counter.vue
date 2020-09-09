4d47cd5a27a1f8c70284c5b12a1dc2cbc42e38c1
Andychuang <bboy81905@gmail.com>
2020-09-09 17:38:35 +0800

    finished style and class binding sample

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="counter">
     <Header msg="I'm counter Page"></Header>
-    <h2 class="text">Number:{{ count }}</h2>
-    <h2 class="text">Roman Number:{{ romanToInt }}</h2>
+    <h2 class="text" v-bind:style="{ color: activeColor, 'font-size': fontSize }">
+      Number:{{ count }}
+    </h2>
+    <h2 :class="['text', { 'red-bigFont': count === 10, 'green-smFont': count === 0 }]">
+      Roman Number:{{ romanToInt }}
+    </h2>
     <div class="btn-group">
       <button v-on:click="increase()">+</button>
       <button @click="decrease()">-</button>
@@ -19,12 +23,24 @@ export default {
   data() {
     return {
       count: 0,
+      activeColor: 'green',
+      fontSize: '12px',
     };
   },
   watch: {
     count(newNumber) {
       if (newNumber === 10 || newNumber === 0) {
         alert(`Number achieve ${newNumber}`);
+      }
+      if (newNumber === 10) {
+        this.activeColor = 'red';
+        this.fontSize = '30px';
+      } else if (newNumber === 0) {
+        this.activeColor = 'green';
+        this.fontSize = '12px';
+      } else {
+        this.activeColor = 'black';
+        this.fontSize = '24px';
       }
     },
   },
@@ -65,5 +81,13 @@ export default {
       margin-left: 0.5rem;
     }
   }
+}
+.red-bigFont {
+  color: red;
+  font-size: 30px;
+}
+.green-smFont {
+  color: green;
+  font-size: 12px;
 }
 </style>
